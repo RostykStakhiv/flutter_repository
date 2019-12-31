121f5f7fddf5aa180a663d011be10699b3e8909b
Rostyslav Stakhiv <rost.stakhiv@gmail.com>
2019-12-31 03:02:40 +0200

    - Added baseUrl getter;

--- lib/network_repositories/network_config.dart
+++ lib/network_repositories/network_config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:uri/uri.dart';
 
 class NetworkConfig {
   final String apiHost;
@@ -7,4 +8,12 @@ class NetworkConfig {
 
   NetworkConfig(
       {@required this.apiHost, @required this.apiScheme, this.apiPort});
+
+  String get baseUrl {
+    final UriBuilder _uriBuilder = UriBuilder();
+    _uriBuilder.scheme = apiScheme;
+    _uriBuilder.host = apiHost;
+    _uriBuilder.port = apiPort;
+    return _uriBuilder.build().toString();
+  }
 }
